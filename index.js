6988704c5c897b86f155614601084c66122e1e17
David Ratajczak <david@mockra.com>
2016-03-02 19:19:34 -0800

    updates subreddit list

--- index.js
+++ index.js
@@ -12,12 +12,18 @@ mongoose.connection.on('error', function() {
 var subreddits = [
   { name: 'emberjs', limit: 1 },
   { name: 'elixir', limit: 1 },
+  { name: 'elm', limit: 1 },
   { name: 'games', limit: 1 },
   { name: 'javascript', limit: 2 },
+  { name: 'leagueoflegends', limit: 2 },
   { name: 'node', limit: 2 },
   { name: 'programming', limit: 1 },
+  { name: 'ps4', limit: 1 },
   { name: 'rails', limit: 1 },
-  { name: 'ruby', limit: 1 }
+  { name: 'reactjs', limit: 1 },
+  { name: 'ruby', limit: 1 },
+  { name: 'streetfighter', limit: 1 },
+  { name: 'webdev', limit: 1 }
 ]
 
 function postTweet(sub, post) {
