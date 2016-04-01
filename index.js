765fa093fe7a6d137c2cc985e935f15849844a1c
David Ratajczak <david@mockra.com>
2016-04-01 10:05:21 -0700

    adds streetfighter subreddit

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ var subreddits = [
   { name: 'rails', limit: 1 },
   { name: 'reactjs', limit: 1 },
   { name: 'ruby', limit: 1 },
+  { name: 'streetfighter', limit: 1 },
   { name: 'webdev', limit: 1 }
 ]
 
