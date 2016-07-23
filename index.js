6bc3eef0334fa27bdad1ed5ff10b53cc7e6cc737
David Ratajczak <david@mockra.com>
2016-07-22 17:43:42 -0700

    adds pokemon go subreddits

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ var subreddits = [
   { name: 'javascript', limit: 2 },
   { name: 'leagueoflegends', limit: 2 },
   { name: 'node', limit: 1 },
+  { name: 'pokemongo', limit: 1 },
   { name: 'programming', limit: 1 },
   { name: 'ps4', limit: 1 },
   { name: 'rails', limit: 1 },
   { name: 'reactjs', limit: 1 },
   { name: 'ruby', limit: 1 },
+  { name: 'TheSilphRoad', limit: 1 },
   { name: 'summonerschool', limit: 1 },
   { name: 'webdev', limit: 1 }
 ]
